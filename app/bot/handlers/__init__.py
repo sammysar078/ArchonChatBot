887d0388a26515d655cb20731da54847922fcca9
sammysar078 <77676249+sammysar078@users.noreply.github.com>
2026-08-11 14:15:01 +0530

    Update __init__.py

--- app/bot/handlers/__init__.py
+++ app/bot/handlers/__init__.py
@@ -7,6 +7,7 @@ from .sudo import router as sudo_router
 from .permissions import router as permissions_router
 from .maintenance import router as maintenance_router
 from .broadcast import router as broadcast_router
+from .voice import router as voice_router
 from .group import router as group_router
 from .chat import router as chat_router
 from .events import router as events_router
@@ -19,6 +20,7 @@ router.include_router(sudo_router)
 router.include_router(permissions_router)
 router.include_router(maintenance_router)
 router.include_router(broadcast_router)
+router.include_router(voice_router)
 router.include_router(group_router)
 router.include_router(chat_router)
 router.include_router(events_router)
